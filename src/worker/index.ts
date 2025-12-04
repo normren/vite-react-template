1138551a09bdb00798845b93edbdb4d05211e06e
NormRen <normren@gmail.com>
2025-12-04 18:12:51 +0800

    remove strict

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
-const app = new Hono<{ Bindings: Env }>({strict: false});
+const app = new Hono<{ Bindings: Env }>();
 
 app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
-app.get("/api/getInfo/", (c) => c.json({ name: "GetInfo" }));
+app.get("/api/getInfo", (c) => c.json({ name: "GetInfo" }));
 
 export default app;
