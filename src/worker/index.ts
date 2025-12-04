e5da00b42bdab1bcc09d90cda947d29b7cfe485b
NormRen <normren@gmail.com>
2025-12-04 17:58:05 +0800

    Add new endpoint '/api/getInfo' to the app

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -2,5 +2,6 @@ import { Hono } from "hono";
 const app = new Hono<{ Bindings: Env }>();
 
 app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
+app.get("/api/getInfo", (c) => c.json({ name: "GetInfo" }));
 
 export default app;
